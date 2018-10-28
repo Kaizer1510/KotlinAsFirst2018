dea70edff3c1a49143635e3eb7618c914a0bd30c
Kaizer1510 <43118860+Kaizer1510@users.noreply.github.com>
2018-10-28 23:53:57 +0400

    Update List.kt

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -117,11 +117,9 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double {
-    var result = 0.0
-    for (element in v) result += pow(element, 2)
-    return sqrt(result)
-}
+fun abs(v: List<Double>): Double = sqrt(v.fold(0.0) { pv, el ->
+    pv + pow(el, 2)
+})
 
 /**
  * Простая
